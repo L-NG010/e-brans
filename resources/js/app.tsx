658207bdf54a09e68c363eee081c3dfee79013f7
L-NG010 <langnatanegara73@gmail.com>
2025-07-26 23:44:20 +0700

    init aos

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,6 +4,10 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
